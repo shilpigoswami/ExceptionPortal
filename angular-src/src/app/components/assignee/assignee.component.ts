6d179d3012b82f3ebcebbc4e2e2797e902e80d50
shilpigoswami <goswamishilpi11@gmail.com>
2019-11-14 05:49:08 +0530

    getting tickets based on ticket type and violation type from express API

--- angular-src/src/app/components/assignee/assignee.component.ts
+++ angular-src/src/app/components/assignee/assignee.component.ts
@@ -7,24 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssigneeComponent implements OnInit {
 
-  // tryout start from Here
-  private map = new Map<string, string[]>([
-    ['IMR', ['Resolution SLA']],
-    ['AHD SR', ['Response SLA', 'Resolution SLA', 'Re-Opened SLA']],
-  ])
-
-  TT: string;
-  VT: string;
-
-  get tts(): string[] {
-    return Array.from(this.map.keys());
-  }
-
-  get vts(): string[] | undefined {
-    return this.map.get(this.TT);
-  }
-
-// till
   constructor() { }
 
   ngOnInit() {
@@ -38,17 +20,20 @@ export class AssigneeComponent implements OnInit {
   violationSR = [{name:"Response SLA", value:1}, {name:"Resolution SLA", value:2},
                       {name:"Re-opened SR", value:2}]
   violationIMR = [{name:"Resolution SLA", value:1}]
+  violations = []
 
   selectedTicket(){
     console.log(this.selectedTicketType);
     if(this.selectedTicketType!=null){
       this.violationTypeDisabled = false;
     }
-    if(this.selectedTicketType=="AHD SR"){
-
+    if(this.selectedTicketType.name =="AHD SR"){
+        this.violations = this.violationSR;
+        console.log(this.selectedTicketType);
     }
-    else if(this.selectedTicketType=="IMR"){
-
+    else if(this.selectedTicketType.name =="IMR"){
+        this.violations = this.violationIMR;
+        console.log(this.selectedTicketType );
     }
   }
 
